0bff3828594155d85c3d26db3a9b45c38d9014a8
Manuel Martinez <manuel.martinez@presidencia.gob.ar>
2023-05-22 13:20:44 -0300

    Agregar archivos de proyecto.

---
+++ Punto 12/Contexto/IsteaContext.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Punto_12.Models;
+
+namespace Punto_12.Contexto
+{
+    public partial class IsteaContext : DbContext
+    {
+        public IsteaContext()
+        {
+        }
+
+        public IsteaContext(DbContextOptions<IsteaContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Persona> Personas { get; set; }
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Persona>(entity =>
+            {
+                entity.Property(e => e.Id).HasColumnName("id");
+
+                entity.Property(e => e.Apellido)
+                    .IsRequired()
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Nombre)
+                    .IsRequired()
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
---
+++ Punto 12/Controllers/PersonaController.cs
@@ -0,0 +1,76 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Punto_12.Contexto;
+using Punto_12.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Punto_12.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class PersonaController : ControllerBase
+    {
+        private readonly IsteaContext _contexto;
+        public PersonaController(IsteaContext isteaContext)
+        {
+            _contexto = isteaContext;
+        }
+
+        [HttpGet]
+        public IEnumerable<Persona> Get()
+        {
+            return _contexto.Personas.ToList();
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<Persona> Get(int id)
+        {
+            Persona persona = _contexto.Personas.FirstOrDefault(x => x.Id == id);
+            if (persona == null)
+            {
+                return NotFound();
+            }
+            return persona;
+        }
+
+        [HttpPost]
+        public ActionResult<Persona> Post([FromBody] Persona value)
+        {
+            _contexto.Personas.Add(value);
+            _contexto.SaveChanges();
+
+            return value;     
+        }
+
+        [HttpPut("{id}")]
+        public ActionResult Put(int id, [FromBody] Persona value)
+        {
+            if (id != value.Id)
+            {
+                return BadRequest();
+            }
+            _contexto.Entry(value).State = EntityState.Modified;
+            _contexto.SaveChanges();
+            return Ok();
+
+        }
+
+        [HttpDelete("{id}")]
+        public ActionResult<Persona> Delete(int id)
+        {
+            Persona persona = _contexto.Personas.FirstOrDefault(a => a.Id == id);
+
+            if (persona == null)
+            {
+                return NotFound();
+            }
+            _contexto.Personas.Remove(persona);
+            _contexto.SaveChanges();
+            return persona;
+        }
+    }
+}
